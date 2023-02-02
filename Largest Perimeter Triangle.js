25a8258924bf00fc66a8f7faec7524e882089275
Omar Ahmed <120424034+omarabozied5@users.noreply.github.com>
2023-02-02 17:33:05 +0200

    Add files via upload

---
+++ Largest Perimeter Triangle.js
@@ -0,0 +1,9 @@
+var largestPerimeter = function(nums) {
+    nums.sort((a,b)=>a-b);
+    for(let i=nums.length-1; i>=0; i--){
+        if(nums[i]<nums[i-1]+nums[i-2]){
+            return nums[i]+nums[i-1]+nums[i-2];
+        }
+    }
+    return 0;
+};
